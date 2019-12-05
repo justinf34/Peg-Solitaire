b779211b390d2d3b7b65d3c670345d9dc2709869
justinf34 <justin.flores@ucalgary.ca>
2019-12-04 21:12:06 -0700

    Implemented pagoda and independence checking.

--- My_pegSolitaire.pl
+++ My_pegSolitaire.pl
@@ -22,41 +22,57 @@ fullBoard([0,1,2,3,4,5,6,
 
 
 % Board States
-board(crossbow, [31,32,34,35,41,42,43,44,45,53], [3]).
-board(longbow, [20,30,31,41,33,43,35,45,26,36,52,53,54,63], [3]).
-board(not_quite_dead, [2,3,4,12,14,20,21,22,23,24,25,26,30,32,35,36,40,41,42,43,44,45,46,52,54,62,64], [33]).
+% Full board
+board(full, [
+        2,  3,  4,           
+       12, 13, 14,
+20, 21, 22, 23, 24, 25, 26,
+30, 31, 32,     34, 35, 36,
+40, 41, 42, 43, 44, 45, 46,
+       52, 53, 54,
+       62, 63, 64
+         ], [33]).
+
+% Crossbow board
+board(crossbow, [31,32, 34, 35, 41, 42, 43, 44, 45, 53], [3]).
+
+% Longbow board
+board(longbow, [20, 26, 30, 31, 33, 35 ,36, 41, 43, 45, 52, 53, 54,63], [3]).
+
+% Not quite dead board
+board(notquitedead, [
+        2, 3,   4,           
+       12,     14,
+20, 21, 22, 23, 24, 25, 26,
+30,     32,         35, 36,
+40, 41, 42, 43, 44, 45, 46,
+       52,     54,
+       62,     64
+           ], [33]).
+
+% Half dead board
+board(halfdead, [
+20,     22, 23, 24, 
+30,             34, 35,
+40, 41, 42, 43, 44, 45,
+       52,     54,
+       62,     64
+           ], [33]).
+
+% Not dead board
+board(notdead, [
+       22, 23, 24, 
+               34, 35,
+       42, 43, 44
+                   ], [33]).
 
 
 %----- Pagodas -----%
 % Simple
-pagoda(simple,2,0).
-pagoda(simple,3,0).
-pagoda(simple,4,0).
-pagoda(simple,12,0).
 pagoda(simple,13,1).
-pagoda(simple,14,0).
-pagoda(simple,20,0).
-pagoda(simple,21,0).
-pagoda(simple,22,0).
-pagoda(simple,23,0).
-pagoda(simple,24,0).
-pagoda(simple,25,0).
-pagoda(simple,26,0).
-pagoda(simple,30,0).
 pagoda(simple,31,1).
-pagoda(simple,32,0).
 pagoda(simple,33,1).
-pagoda(simple,34,0).
 pagoda(simple,35,1).
-pagoda(simple,36,0).
-pagoda(simple,40,0).
-pagoda(simple,41,0).
-pagoda(simple,42,0).
-pagoda(simple,43,0).
-pagoda(simple,44,0).
-pagoda(simple,45,0).
-pagoda(simple,46,0).
-pagoda(simple,52,0).
 pagoda(simple,53,1).
 pagoda(simple,54,0).
 pagoda(simple,62,0).
@@ -64,65 +80,107 @@ pagoda(simple,63,0).
 pagoda(simple,64,0).
 
 %Asymmetric
-pagoda(asym,2,0).
-pagoda(asym,3,0).
-pagoda(asym,4,0).
-pagoda(asym,12,0).
 pagoda(asym,13,1).
-pagoda(asym,14,0).
-pagoda(asym,20,-1).
+pagoda(asym,20,(-1)).
 pagoda(asym,21,1).
-pagoda(asym,22,0).
 pagoda(asym,23,1).
-pagoda(asym,24,0).
 pagoda(asym,25,1).
-pagoda(asym,26,-1).
-pagoda(asym,30,0).
+pagoda(asym,26,(-1)).
 pagoda(asym,31,2).
-pagoda(asym,32,0).
 pagoda(asym,33,2).
-pagoda(asym,34,0).
 pagoda(asym,35,2).
-pagoda(asym,36,0).
-pagoda(asym,40,-1).
+pagoda(asym,40,(-1)).
 pagoda(asym,41,1).
-pagoda(asym,42,0).
 pagoda(asym,43,1).
-pagoda(asym,44,0).
 pagoda(asym,45,1).
-pagoda(asym,46,-1).
-pagoda(asym,52,0).
+pagoda(asym,46,(-1)).
 pagoda(asym,53,1).
-pagoda(asym,54,0).
-pagoda(asym,62,0).
-pagoda(asym,63,0).
-pagoda(asym,64,0).
+
+
+% Asymmetric Rotated 90 degrees
+pagoda(asym90,2,(-1)).
+pagoda(asym90,4,(-1)).
+pagoda(asym90,12,1).
+pagoda(asym90,13,2).
+pagoda(asym90,14,1).
+pagoda(asym90,31,1).
+pagoda(asym90,32,1).
+pagoda(asym90,33,2).
+pagoda(asym90,34,1).
+pagoda(asym90,35,1).
+pagoda(asym90,52,1).
+pagoda(asym90,53,2).
+pagoda(asym90,54,1).
+pagoda(asym90,62,(-1)).
+pagoda(asym90,64,(-1)).
+
+% Strong
+pagoda(strong,3,21).
+pagoda(strong,13,13).
+pagoda(strong,20,(-8)).
+pagoda(strong,21,8).
+pagoda(strong,23,8).
+pagoda(strong,25,8).
+pagoda(strong,26,(-8)).
+pagoda(strong,30,5).
+pagoda(strong,31,5).
+pagoda(strong,33,5).
+pagoda(strong,35,5).
+pagoda(strong,36,5).
+pagoda(strong,40,(-3)).
+pagoda(strong,41,3).
+pagoda(strong,43,3).
+pagoda(strong,45,3).
+pagoda(strong,46,(-3)).
+pagoda(strong,53,2).
+pagoda(strong,63,1).
+
 
 
 wgt(_,[],0).
 wgt(P, [Pos|Rest], Wgt) :-
-    (pagoda(P,Pos,Wgt);
-    PWgt = 0), !,
+    (pagoda(P,Pos,PWgt);
+    PWgt = 0), 
+    !,
     wgt(P, Rest, WgtRest),
     Wgt is WgtRest + PWgt.
 
-goal_wgt(crossbow, simple, 1).
+
+
+check_wgts(_,[]).
+check_wgts(Game, [(Pagoda,WgtP)|Rest]) :-
+    board(Game,_,Goal),
+    wgt(Pagoda,Goal, WgtGoal),
+    WgtP >= WgtGoal,
+    check_wgts(Game,Rest).
 
 
 
-% % Independence check
-% independent(_, []).
-% independent(Mv, [H|T]) :-
-%     overlap(Mv,H), !.
-% independent(Mv, [H|T]) :-
-%     lexorder(Mv,H),
-%     independent(Mv, T).
+% Independence check
+independence_check(_, []).
+independence_check(Mv, [H|_]) :-
+    overlap(Mv,H), !.
+independence_check(Mv, [H|T]) :-
+    lexorder(Mv,H),
+    independence_check(Mv, T).
 
-% overlap((S1,E1), (S2,E2)) :-
-%     jump(S1, J1, E2),
-%     jump(S2,J2,E2),
-%     (S1)
+overlap((S1,E1), (S2,E2)) :-
+    jump(S1, J1, E1),
+    jump(S2,J2,E2),
+    (S1 = S2; S1 = J2; S1 = E2;
+    J1 = S2; J1 = J2; J1 = E2;
+    E1 = S2; E1 = J2; E1 = E2;
+    S2 = S1; S2 = J1; S2 = E1;
+    J2 = S1; J2 = J1; J2 = E1;
+    E2 = S1; E2 = J1; E2 = E1).
 
+lexorder((S1,E1), (S2, E2)) :-
+    jump(S1,J1,E1),
+    jump(S2,J2,E2),
+    X is S1 + J1 + E1,
+    Y is S2 + J2 + E2,
+    X =< Y.
+    
 
 % Boarder check
 onboard(Pos) :- 2 =< Pos, Pos =< 4.
@@ -178,18 +236,23 @@ solitaire_steps(SB, [MV|Moves],GB) :-
 solitaire_steps(GB, [], GB).
 
 
+solitaire_stepsModed(Game,B,_,[]) :-
+    board(Game,_,B).
+
+solitaire_stepsModed(Game,B,Hist,[Mv|Moves]) :-
+    solitaire_move(B, Mv, NewBoard),
+    independence_check(Mv,Hist),
+    findall((P,W), (member(P,[asym,asym90,strong]), wgt(P,NewBoard, W)), Wgts),
+    check_wgts(Game, Wgts),
+    solitaire_stepsModed(Game,NewBoard,[Mv|Hist], Moves), !.
+
 peg(Board) :-
-    board(Board, Start, Goal),
-    solitaire_steps(Start, Moves, Goal),
+    board(Board, Start, _),
+    solitaire_stepsModed(Board,Start, [], Moves),
     write(Moves), nl,
     write("Starting State"),nl,
     makeBoard(Start,Goal,B), wBoard(B),
-    displayBoard2(Start,Moves,Goal).
-
-
-
-
-    
+    displayBoard2(Start,Moves,Goal), !.
 
 
 % ---------- Show Board ----------%
